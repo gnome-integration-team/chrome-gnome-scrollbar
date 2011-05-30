c3cb0a23df06348d8d137848f49dabf1c2d93294
Garrett LeSage <garrett@lesage.us>
2011-05-30 23:34:24 +0200

    update name in CSS/JS

--- js/scrollbars.js
+++ js/scrollbars.js
@@ -3,7 +3,7 @@ if (window.location.host == "wave.google.com") {
 	for (i = 0; i < document.styleSheets.length; ++i) {
 		css = document.styleSheets[i];
 		if (css.rules.length &&
-		 	".light_themes_scrollbars" == css.rules[0].selectorText) {
+		 	".adwaita_themes_scrollbars" == css.rules[0].selectorText) {
 			css.disabled = true;
 		}
 	}
